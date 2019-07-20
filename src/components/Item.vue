cb14c2111170bf93c84c88d1b5dafba3c39afbb5
2228787638 <2228787638@qq.com>
2019-07-20 19:06:53 +0800

    [feat:implement the to do list]

---
+++ src/components/Item.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <p v-for="item in items">
+            <label><input type="checkbox"/>{{item.name}}</label>
+        </p>
+
+    </div>
+</template>
+
+<script>
+    /* eslint-disable vue/no-shared-component-data,no-unused-vars,no-console,no-undef */
+    import Bus from '../bus';
+    export default {
+        name: "Item",
+        data(){
+            return {
+                items:[
+                    {
+                        name: 1,
+                        active: true
+                    },
+                    {
+                        name: 2,
+                        active: true
+                    },
+                    {
+                        name: 3,
+                        active: true
+                    }
+                ]
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
